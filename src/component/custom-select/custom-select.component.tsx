ddc5e65c4ed53163effe99e3c35fa4b419f24867
Aleksa Cekic <aleksa.cekic8@gmail.com>
2022-04-07 01:01:30 +0200

    added tailwind css for styling, created services and started working on form component

--- src/component/custom-select/custom-select.component.tsx
+++ src/component/custom-select/custom-select.component.tsx
@@ -5,15 +5,20 @@ type CustomSelectProps = {
     handleChange: (event: any) => void;
 }
 
-const CustomSelect = ({children, handleChange}: CustomSelectProps) => (
-    <select
-        className="w-24 border border-gray-300 px-3 py-2 rounded-lg bg-white shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 appearance-none"
-        onChange={handleChange}
-    >
-        {
-            children?.map(child => <option key={child} value={child}>{child}</option>)
-        }
-    </select>
-)
+const CustomSelect = ({children, handleChange}: CustomSelectProps) => {
+
+    return (
+        <select
+            className="w-24 border border-gray-300 px-3 py-2 rounded-lg bg-white shadow-sm focus:outline-none focus:border-indigo-500 focus:ring-1 focus:ring-indigo-500 appearance-none"
+            onChange={handleChange}
+        >
+            {
+                children?.map(child => <option key={child} value={child}>
+                    {child}
+                </option>)
+            }
+        </select>
+    )
+}
 
 export default CustomSelect;
