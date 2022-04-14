888aa33cc2ea81211bf0b586373f1cba5681c09c
Aleksa Cekic <aleksa.cekic8@gmail.com>
2022-04-15 00:30:29 +0200

    added responsive design

--- src/pages/weather/weather.component.tsx
+++ src/pages/weather/weather.component.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./weather.styles.scss";
 import WeatherForm from "../../component/weather-form/weather-form.component";
 import Forecast from "../../component/forecast/forecast-component";
@@ -7,6 +7,31 @@ import {getWeather, getWeatherFor7Days} from "../../services/weather.service";
 const Weather = () => {
     const [weather, setWeather] = useState(null);
     const [avgTemperature, setAvgTemperature] = useState(0);
+    const [bg, setBg] = useState("");
+
+    useEffect(() => {
+        if (avgTemperature === 0) {
+            setBg("linear-gradient(90deg, rgba(156,221,253,1))")
+        } else if (avgTemperature > 0 && avgTemperature < 10) {
+            setBg("linear-gradient(90deg, rgba(156, 221, 253, 1) 50%, rgba(255, 214, 108, 1) 100%)")
+        } else if (avgTemperature > 10 && avgTemperature < 20) {
+            setBg("linear-gradient(90deg, rgba(156, 221, 253, 1) 50%, rgba(255, 214, 108, 1) 100%)")
+        } else if (avgTemperature > 20 && avgTemperature < 30) {
+            setBg("linear-gradient(90deg, rgba(255, 214, 108, 1) 50%, rgba(156, 221, 253, 1) 100%)")
+        } else if (avgTemperature > 30) {
+            setBg("linear-gradient(90deg, rgba(156, 221, 253, 1) 100%")
+        } else if (avgTemperature < 0 && avgTemperature > -10) {
+            setBg("linear-gradient(90deg, rgba(23, 156, 231, 1) 50%, rgba(156, 221, 253, 1) 100%)")
+        } else if (avgTemperature < -10 && avgTemperature > -20) {
+            setBg("linear-gradient(90deg, rgba(13, 60, 138, 1) 50%, rgba(23, 156, 231, 1) 100%)")
+        } else if (avgTemperature > 0 && avgTemperature < 40) {
+            setBg("linear-gradient(90deg, rgba(156, 221, 253, 1) 0%, rgba(254, 152, 90, 1) 100%)")
+        } else if (avgTemperature > 0 && avgTemperature < 40) {
+            setBg("linear-gradient(90deg, rgba(156, 221, 253, 1) 0%, rgba(254, 152, 90, 1) 100%)")
+        } else if (avgTemperature < 0 && avgTemperature >= -40) {
+            setBg("linear-gradient(90deg, rgba(13,60,138,1) 0%, rgba(156, 221, 253, 1) 50%)")
+        }
+    }, [avgTemperature]);
 
     const addWeather = (country: string, city: string) => {
         getWeather(country, city).then(data => {
@@ -18,7 +43,7 @@ const Weather = () => {
     }
 
     const getAverageTemperature = (w: any) => {
-        let temps = w.daily.slice(1).map((w: any) => w.temp.day);
+        let temps = w.daily.slice(2).map((w: any) => w.temp.day);
 
         let sum = temps.reduce((sum: number, num: number) => sum + num, 0);
         let mean = sum / temps.length;
@@ -27,7 +52,7 @@ const Weather = () => {
     }
 
     const style = {
-        background: "whitesmoke"
+        background: bg
     }
 
     return (
